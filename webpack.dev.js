d2d71726223e2b37b81b30747db524113fab69ce
dreyWesson <dreyWesson@gmail.com>
2020-03-25 10:53:54 +0100

    fixing logo

--- webpack.dev.js
+++ webpack.dev.js
@@ -11,7 +11,7 @@ module.exports = merge(common, {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: "./src/template.html",
       favicon: "./src/assets/favicon.png"
     })
   ],
